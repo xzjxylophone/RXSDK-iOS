a8f1b03826cb5c48481c41774794b999830d724a
Rush <xzjxylophone@126.com>
2018-02-22 16:38:09 +0800

    add

---
+++ RXSDK/RXSDK.framework/Headers/RXSDK.h
@@ -0,0 +1,20 @@
+//
+//  RXSDK.h
+//  RXSDK
+//
+//  Created by Rush.D.Xzj on 07/02/2018.
+//  Copyright © 2018 TAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RXSDK.
+FOUNDATION_EXPORT double RXSDKVersionNumber;
+
+//! Project version string for RXSDK.
+FOUNDATION_EXPORT const unsigned char RXSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXSDK/PublicHeader.h>
+
+#import "RXSDKHeader.h"
+
---
+++ RXSDK/RXSDK.framework/Headers/RXSDKHeader.h
@@ -0,0 +1,16 @@
+//
+//  RXSDKHeader.h
+//  RXSDK
+//
+//  Created by Rush.D.Xzj on 07/02/2018.
+//  Copyright © 2018 TAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RXSDK : NSObject
+
++ (UIView *)rxsdkView;
+
+
+@end
